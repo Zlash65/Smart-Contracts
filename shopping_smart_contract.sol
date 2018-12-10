fb9ddbe368d00230824979208a8fda48bd52668a
Zlash65 <zarrar65@gmail.com>
2018-12-11 00:50:45 +0530

    product add function added, get product count added

--- shopping_smart_contract.sol
+++ shopping_smart_contract.sol
@@ -5,7 +5,7 @@ contract shoppingSmartContract {
     // struct to hold various kind of data for seller
 	struct Seller {
 		address sellerAddress;
-		uint32[] productId;
+		uint32[] productsData;
 		bool whitelisted;
 	}
     
@@ -16,7 +16,7 @@ contract shoppingSmartContract {
 	}
     
 	mapping(address => Seller) sellers;
-	mapping(uint32 => Productt) products;
+	mapping(uint32 => Product) products;
 	address admin;
 	uint32 productCount;
 
@@ -26,7 +26,7 @@ contract shoppingSmartContract {
 	}
 
 	modifier onlyWhitelistedSeller() {
-		require(sellers[msg.sender].sellerAddress != "", "Whitelisted seller can add products");
+		require(sellers[msg.sender].sellerAddress != msg.sender, "Only whitelisted Seller can add products");
 		_;
 	}
     
@@ -38,16 +38,31 @@ contract shoppingSmartContract {
 	// public visible function to register yourself as seller
 	function addAsSeller() public returns (bool status) {
 		// check if seller already registered
-		require(sellers[msg.sender].sellerAddress!="", "Already added as seller.");
+		require(sellers[msg.sender].sellerAddress == msg.sender, "Already added as seller.");
 
 		Seller memory seller = Seller(msg.sender, new uint32[](0), false);
 		sellers[msg.sender] = seller;
 	}
 
 	function makeSellerWhitelisted(address sellerId) public onlyAdmin {
-		require(sellers[sellerId].sellerAddress!="", "Seller with the given address does not exist.");
+		require(sellers[sellerId].sellerAddress != msg.sender, "Seller with the given address does not exist.");
 
 		sellers[sellerId].whitelisted = true;
+	}
+
+	function addProduct(uint32 _id, uint256 _price) public onlyWhitelistedSeller {
+		// check if product already added
+		require(products[_id].productId == _id, "Product with the given id already exist.");
+
+		Product memory prod = Product(_id, _price);
+		products[_id] = prod;
+		Seller memory seller = sellers[msg.sender];
+		seller.productsData.push(prod);
+		productCount++;
+	}
+
+	function getProductCount() public onlyAdmin returns (uint32) {
+		return productCount;
 	}
 
 }
