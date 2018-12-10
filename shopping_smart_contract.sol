7bdebf0d4bb942329ae31a20bd72ecb80d31b540
Zlash65 <zarrar65@gmail.com>
2018-12-11 01:43:30 +0530

    minor improvement to optimise gas consumption and other fixes

--- shopping_smart_contract.sol
+++ shopping_smart_contract.sol
@@ -5,18 +5,18 @@ contract shoppingSmartContract {
     // struct to hold various kind of data for seller
 	struct Seller {
 		address sellerAddress;
-		uint32[] productsData;
+		bytes32[] productsData;
 		bool whitelisted;
 	}
     
     // struct to hold product related data
 	struct Product {
-		uint32 productId;
+		bytes32 productId;
 		uint256 price;
 	}
     
 	mapping(address => Seller) sellers;
-	mapping(uint32 => Product) products;
+	mapping(bytes32 => Product) products;
 	address admin;
 	uint32 productCount;
 
@@ -26,38 +26,40 @@ contract shoppingSmartContract {
 	}
 
 	modifier onlyWhitelistedSeller() {
-		require(sellers[msg.sender].sellerAddress != msg.sender, "Only whitelisted Seller can add products");
+		require(sellers[msg.sender].sellerAddress != address(0), "Seller does not exist");
+		require(sellers[msg.sender].whitelisted == true, "Only whitelisted Seller can add products");
 		_;
 	}
     
     // make creator of contract as admin
-	constructor(shoppingSmartContract) internal {
+	constructor() public {
 		admin = msg.sender;
 	}
 
 	// public visible function to register yourself as seller
-	function addAsSeller() public returns (bool status) {
+	function addAsSeller() public {
 		// check if seller already registered
-		require(sellers[msg.sender].sellerAddress == msg.sender, "Already added as seller.");
+		require(sellers[msg.sender].sellerAddress != msg.sender, "Already added as seller.");
 
-		Seller memory seller = Seller(msg.sender, new uint32[](0), false);
+		Seller memory seller = Seller(msg.sender, new bytes32[](0), false);
 		sellers[msg.sender] = seller;
 	}
 
 	function makeSellerWhitelisted(address sellerId) public onlyAdmin {
 		require(sellers[sellerId].sellerAddress != msg.sender, "Seller with the given address does not exist.");
+		require(sellers[sellerId].sellerAddress != address(0), "Seller with the given address does not exist.");
 
 		sellers[sellerId].whitelisted = true;
 	}
 
-	function addProduct(uint32 _id, uint256 _price) public onlyWhitelistedSeller {
+	function addProduct(bytes32 _id, uint256 _price) public onlyWhitelistedSeller {
 		// check if product already added
-		require(products[_id].productId == _id, "Product with the given id already exist.");
+		require(products[_id].productId != _id, "Product with the given id already exist.");
 
 		Product memory prod = Product(_id, _price);
 		products[_id] = prod;
-		Seller memory seller = sellers[msg.sender];
-		seller.productsData.push(prod);
+// 		Seller memory seller = sellers[msg.sender];
+// 		seller.productsData.push(prod);
 		productCount++;
 	}
 
