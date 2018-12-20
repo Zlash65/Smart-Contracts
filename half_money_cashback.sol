5e6c3bc634353c3bb566944857d304a3424fb8b8
Zlash65 <zarrar65@gmail.com>
2018-12-20 11:07:50 +0530

    added logs to cashback contract

--- half_money_cashback.sol
+++ half_money_cashback.sol
@@ -1,15 +1,22 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract sendReceive {
+
+	// Logs
+	event eventsLog(bytes32 message, address _address, uint _amount);
     
     // initiates a contract to receive donation
 	function receiveDonation() public payable {
+		emit eventsLog("Amount Received", msg.sender, msg.value);
+
 		donateBackHalf(msg.value);
 	}
     
 	// initiate a return of hald the donation received
 	function donateBackHalf(uint amount) private {
 		msg.sender.transfer(amount/2);
+
+		emit eventsLog("Cashback sent", msg.sender, amount/2);
 	}
     
 }
